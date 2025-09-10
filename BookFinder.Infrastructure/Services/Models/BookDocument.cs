4bdf749f552e6100b94eba9bd7f33eaed150c19b
josiasdev <francisco.batista67@alu.ufc.br>
2025-09-10 10:05:59 -0300

    feat: Modelo das propriedas do json.

--- BookFinder.Infrastructure/Services/Models/BookDocument.cs
+++ BookFinder.Infrastructure/Services/Models/BookDocument.cs
@@ -1,6 +1,21 @@
+using System.Text.Json.Serialization;
+
 namespace BookFinder.Infrastructure.Services.Models;
 
 public class BookDocument
 {
-    
+    [JsonPropertyName("key")]
+    public string Key { get; set; } = string.Empty;
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; } = string.Empty;
+
+    [JsonPropertyName("author_name")]
+    public List<string> AuthorName { get; set; } = new();
+
+    [JsonPropertyName("author_key")]
+    public List<string> AuthorKey { get; set; } = new();
+
+    [JsonPropertyName("first_publish_year")]
+    public int? FirstPublishYear { get; set; }
 }
