4aa4a13bafa58d322573e4e2e813788ba69b8e2a
josiasdev <francisco.batista67@alu.ufc.br>
2025-09-10 10:06:37 -0300

    feat: Modelo para procurar resposta no library de pesquisar

--- BookFinder.Infrastructure/Services/Models/OpenLibrarySearchResponse.cs
+++ BookFinder.Infrastructure/Services/Models/OpenLibrarySearchResponse.cs
@@ -1,6 +1,8 @@
+using System.Text.Json.Serialization;
 namespace BookFinder.Infrastructure.Services.Models;
 
 public class OpenLibrarySearchResponse
 {
-    
+    [JsonPropertyName("docs")]
+    public List<BookDocument> Docs { get; set; } = new();
 }
