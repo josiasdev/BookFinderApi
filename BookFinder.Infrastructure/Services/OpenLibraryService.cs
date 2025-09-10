9f49b0e8de25cc69f3e9d27092457cec49034af2
josiasdev <francisco.batista67@alu.ufc.br>
2025-09-10 10:07:25 -0300

    feat: Responsável por buscar autor pelo nome.

--- BookFinder.Infrastructure/Services/OpenLibraryService.cs
+++ BookFinder.Infrastructure/Services/OpenLibraryService.cs
@@ -1,6 +1,31 @@
+using BookFinder.Infrastructure.Services.Models;
+using System.Net.Http.Json;
 namespace BookFinder.Infrastructure.Services;
 
-public class OpenLibraryService
+public class OpenLibraryService : IOpenLibraryService
 {
-    
+    private readonly HttpClient _httpClient;
+
+    public OpenLibraryService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri("https://openlibrary.org/");
+    }
+
+    public async Task<OpenLibrarySearchResponse?> SearchBooksByAuthorAsync(string authorName)
+    {
+        try
+        {
+            var formattedAuthorName = authorName.ToLower().Replace(" ", "+");
+            var requestUri = $"search.json?author={formattedAuthorName}";
+
+            var response = await _httpClient.GetFromJsonAsync<OpenLibrarySearchResponse>(requestUri);
+            return response;
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+            return null;
+        }
+    }
 }
